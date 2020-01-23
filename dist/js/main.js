846e16338d663094b55ca58f859d115b3a60f580
Zoubir-Barraz <zoubirbarraz@gmail.com>
2020-01-23 13:55:02 +0100

    up

--- dist/js/main.js
+++ dist/js/main.js
@@ -12,7 +12,7 @@
         //return r.name === functions(function_choice)
     //})
 
-    //var map = L.map('map').setView([34, -4], 6);
+    var map = L.map('map').setView([34, -4], 6);
 
     L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
         maxZoom: 18,
