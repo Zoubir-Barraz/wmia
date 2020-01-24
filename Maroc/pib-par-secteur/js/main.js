d7863a842ce8093506f67d7043958a090a408836
Zoubir-Barraz <zoubirbarraz@gmail.com>
2020-01-24 22:01:10 +0100

    maps corrected

--- Maroc/pib-par-secteur/js/main.js
+++ Maroc/pib-par-secteur/js/main.js
@@ -1,12 +1,7 @@
 $( document ).ready(function() {
-    var map = L.map('map').setView([32, -6], 4.75);
-
-    L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
-        maxZoom: 18,
-        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
-            '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-            'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-        id: 'mapbox/streets-v11'
+    var map = L.map('map').setView([32, -6], 4);
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
     
     url = "/Maroc/pib-par-secteur/centroideregion.json"
